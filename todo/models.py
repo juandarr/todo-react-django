832f62454106460e96e6efd5598ca8b8ed6536ff
Juan Rios <juanrios@tuta.io>
2023-09-04 00:49:24 -0500

    refactor: views file removing comments and non functional code;add: class meta to reorder todos when retrieved by creation date (ascending)

--- todo/models.py
+++ todo/models.py
@@ -6,8 +6,8 @@ from django.conf import settings
 class List(models.Model):
     id = models.BigAutoField(primary_key=True,unique=True, blank=True)
     title = models.CharField(max_length=75)
-    archived = models.BooleanField(default=False)
-    user = models.ForeignKey(User, on_delete =models.CASCADE, blank=True, null=True, related_name='lists')
+    archived = models.BooleanField(blank=True, default=False)
+    user = models.ForeignKey(User, on_delete =models.CASCADE, blank=True, related_name='lists')
 
     def __str__(self):
         return self.title
@@ -19,14 +19,16 @@ class Todo(models.Model):
     created_at = models.DateTimeField(editable=False, blank=True, default=now)
     complete = models.BooleanField(blank=True, default=False)
 
-    PRIORITIES = (
-        (0, 'Low'),
-        (1, 'Medium'),
-        (2, 'High')
-    )
     priority = models.IntegerField(help_text='Task priority', blank=True,default=0)
+    PRIORITIES = (
+        (0, 'None'),
+        (1, 'Low'),
+        (2, 'Medium'),
+        (3, 'High')
+    )
+
     list = models.ForeignKey(List, on_delete=models.RESTRICT, blank=True, default=1)
-    user = models.ForeignKey(User, on_delete= models.CASCADE, blank=True, null=True, related_name='todos')
+    user = models.ForeignKey(User, on_delete= models.CASCADE, blank=True, related_name='todos')
 
     class Meta:
         ordering = ['created_at']
--- todo/views.py
+++ todo/views.py
@@ -10,52 +10,6 @@ def todo(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
 
-'''        
-class TodoApiView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
-    # 1. List all
-    def get(self, request, *args, **kwargs):
-        todos = Todo.objects.filter(user = request.user.id)
-        serializer_class = TodoSerializer(todos, many=True)
-        return Response(serializer_class.data, status=status.HTTP_200_OK)
-
-    #2. Create
-    def post(self, request, *args, **kwargs):
-        data = {
-            'title': request.data.get('title'),
-            'description': request.data.get('description'),
-            'complete':request.data.get('complete'),
-            'priority': request.data.get('priority'),
-            'list': request.data.get('list')
-        }
-        serializer = TodoSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status = status.HTTP_201_CREATED)
-        return Response(serializer.erros, status = status.HTTP_400_BAD_REQUEST)
-
-class ListApiView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
-
-    # 1. List all
-    def get(self, request, *args, **kwargs):
-        lists = List.objects.filter(user = request.user.id)
-        serializer_class = ListSerializer(lists, many=True)
-        return Response(serializer_class.data, status=status.HTTP_200_OK)
-
-    #2. Create
-    def post(self, request, *args, **kwargs):
-        data = {
-            'title': request.data.get('title'),
-            'archived': request.data.get('archived')
-        }
-        serializer = ListSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status = status.HTTP_201_CREATED)
-        return Response(serializer.erros, status = status.HTTP_400_BAD_REQUEST)
-'''
-
 class TodoApiView(viewsets.ModelViewSet):
     queryset = Todo.objects.all() 
     serializer_class = TodoSerializer
@@ -64,26 +18,18 @@ class TodoApiView(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-    '''
+    
     def get_queryset(self):
         return self.request.user.todos.all()
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
-    '''
 class ListApiView(viewsets.ModelViewSet):
     queryset = List.objects.all() 
     serializer_class = ListSerializer
     authentication_classes = [SessionAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
-    #permission_classes = [permissions.IsAuthenticated]
-    #authentication_classes = []
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
-    '''
-    def get_queryset(self):
-        return self.request.user.lists.all()
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-    '''
+    
+    def get_queryset(self):
+        return self.request.user.lists.all()
