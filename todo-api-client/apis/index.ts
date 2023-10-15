49dc3e053b2df1806b2cfd9580a770bd85fb94ce
Juan Rios <juanrios@tuta.io>
2023-10-15 00:53:55 -0500

    add: static files and open api client to staging branch

---
+++ todo-api-client/apis/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ListsApi';
+export * from './SchemaApi';
+export * from './TodosApi';
+export * from './UsersApi';
---
+++ todo-api-client/models/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './List';
+export * from './PatchedList';
+export * from './PatchedTodo';
+export * from './PatchedUser';
+export * from './Todo';
+export * from './User';
