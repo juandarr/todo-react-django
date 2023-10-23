66b72ac0c75633a59c3b211edef22e03ae00d75a
juandarr <juanrios@tuta.io>
2023-10-23 00:49:04 -0500

    add: basePath to website url instead of local instance

--- jstoolchains/src/lib/api.ts
+++ jstoolchains/src/lib/api.ts
@@ -5,9 +5,9 @@ import {
 } from '../../../todo-api-client/apis/index';
 import { Configuration } from '../../../todo-api-client/runtime';
 import { getCookie } from './utils';
-
+#'http://127.0.0.1:8000'
 const apiConfig = new Configuration({
-	basePath: 'http://127.0.0.1:8000',
+	basePath: 'https://task.bitfusion.link',
 	headers: {
 		'X-CSRFToken': getCookie('csrftoken'),
 	},
