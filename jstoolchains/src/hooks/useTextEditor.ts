a3f305d6b3522f3c3073684ec07901ed8333143e
Juan Rios <juanrios@tuta.io>
2023-10-25 06:38:35 -0500

    feature: added starterKit extension which includes a ton of useful feature while editing text. It is functional now

--- jstoolchains/src/hooks/useTextEditor.ts
+++ jstoolchains/src/hooks/useTextEditor.ts
@@ -1,8 +1,6 @@
-import Document from '@tiptap/extension-document';
-import Paragraph from '@tiptap/extension-paragraph';
+import StarterKit from '@tiptap/starter-kit';
 import TaskItem from '@tiptap/extension-task-item';
 import TaskList from '@tiptap/extension-task-list';
-import Text from '@tiptap/extension-text';
 import CharacterCount from '@tiptap/extension-character-count';
 import Placeholder from '@tiptap/extension-placeholder';
 import { useEditor } from '@tiptap/react';
@@ -15,9 +13,20 @@ export default function useTextEditor(
 ): Editor | null {
 	const editor = useEditor({
 		extensions: [
-			Document,
-			Paragraph,
-			Text,
+			StarterKit.configure({
+				bulletList: {
+					keepMarks: true,
+					keepAttributes: false,
+				},
+				orderedList: {
+					keepMarks: true,
+					keepAttributes: false,
+				},
+				history: false,
+				heading: {
+					levels: [1, 2, 3],
+				},
+			}),
 			TaskList,
 			TaskItem.configure({ nested: true }),
 			Placeholder.configure({
@@ -29,7 +38,36 @@ export default function useTextEditor(
 				limit,
 			}),
 		],
-		content: initialContent,
+		content: ``,
+		// 		<h2>
+		//   Hi there,
+		// </h2>
+		// <p>
+		//   this is a <em>basic</em> example of <strong>tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
+		// </p>
+		// <ul>
+		//   <li>
+		//     That’s a bullet list with one …
+		//   </li>
+		//   <li>
+		//     … or two list items.
+		//   </li>
+		// </ul>
+		// <p>
+		//   Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
+		// </p>
+		// <pre><code class="language-css">body {
+		// display: none;
+		// }</code></pre>
+		// <p>
+		//   I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
+		// </p>
+		// <blockquote>
+		//   Wow, that’s amazing. Good work, boy! 👏
+		//   <br />
+		//   — Mom
+		// </blockquote>
+		// 		`,
 	});
 
 	return editor;
