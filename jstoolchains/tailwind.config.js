7f3fc4a7f14725908c87dd3a393227dea8c8f873
Juan Rios <juanrios@tuta.io>
2024-08-24 18:30:17 -0500

    refactor: changed structure of config file to be ES6 compatible

--- jstoolchains/tailwind.config.js
+++ jstoolchains/tailwind.config.js
@@ -1,88 +1,90 @@
 /** @type {import('tailwindcss').Config} */
-export const darkMode = ['class'];
-export const content = [
-	'../templates/*.html',
-	'../templates/**/*.html',
-	'./src/*.{tsx,ts,js,jsx}',
-	'./src/**/*.{tsx,ts,js,jsx}',
-];
-export const theme = {
-	container: {
-		center: true,
-		padding: '2rem',
-		screens: {
-			'2xl': '1400px',
+module.exports = {
+	darkMode: ['class'],
+	content: [
+		'../templates/*.html',
+		'../templates/**/*.html',
+		'./src/*.{tsx,ts,js,jsx}',
+		'./src/**/*.{tsx,ts,js,jsx}',
+	],
+	theme: {
+		container: {
+			center: true,
+			padding: '2rem',
+			screens: {
+				'2xl': '1400px',
+			},
+		},
+		fontFamily: {
+			Noto: ['Noto Sans', 'Segoe UI Emoji', 'Apple Color Emoji'],
+			Maple: ['Maple', 'Noto Sans']
+		},
+		extend: {
+			spacing: {
+				'75%': '75%',
+				'70%': '70%',
+				'65%': '65%',
+				'50%': '50%',
+				'35%': '35%',
+				'30%': '30%',
+				'25%': '25%',
+				'20%': '20%',
+			},
+			colors: {
+				border: 'hsl(var(--border))',
+				input: 'hsl(var(--input))',
+				ring: 'hsl(var(--ring))',
+				background: 'hsl(var(--background))',
+				foreground: 'hsl(var(--foreground))',
+				primary: {
+					DEFAULT: 'hsl(var(--primary))',
+					foreground: 'hsl(var(--primary-foreground))',
+				},
+				secondary: {
+					DEFAULT: 'hsl(var(--secondary))',
+					foreground: 'hsl(var(--secondary-foreground))',
+				},
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive))',
+					foreground: 'hsl(var(--destructive-foreground))',
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted))',
+					foreground: 'hsl(var(--muted-foreground))',
+				},
+				accent: {
+					DEFAULT: 'hsl(var(--accent))',
+					foreground: 'hsl(var(--accent-foreground))',
+				},
+				popover: {
+					DEFAULT: 'hsl(var(--popover))',
+					foreground: 'hsl(var(--popover-foreground))',
+				},
+				card: {
+					DEFAULT: 'hsl(var(--card))',
+					foreground: 'hsl(var(--card-foreground))',
+				},
+			},
+			borderRadius: {
+				lg: `var(--radius)`,
+				md: `calc(var(--radius) - 2px)`,
+				sm: 'calc(var(--radius) - 4px)',
+			},
+			keyframes: {
+				'accordion-down': {
+					from: { height: 0 },
+					to: { height: 'var(--radix-accordion-content-height)' },
+				},
+				'accordion-up': {
+					from: { height: 'var(--radix-accordion-content-height)' },
+					to: { height: 0 },
+				},
+			},
+			animation: {
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out',
+			},
 		},
 	},
-	fontFamily: {
-		Noto: ['Noto Sans', 'Segoe UI Emoji', 'Apple Color Emoji'],
-		Maple: ['Maple', 'Noto Sans']
-	},
-	extend: {
-		spacing: {
-			'75%': '75%',
-			'70%': '70%',
-			'65%': '65%',
-			'50%': '50%',
-			'35%': '35%',
-			'30%': '30%',
-			'25%': '25%',
-			'20%': '20%',
-		},
-		colors: {
-			border: 'hsl(var(--border))',
-			input: 'hsl(var(--input))',
-			ring: 'hsl(var(--ring))',
-			background: 'hsl(var(--background))',
-			foreground: 'hsl(var(--foreground))',
-			primary: {
-				DEFAULT: 'hsl(var(--primary))',
-				foreground: 'hsl(var(--primary-foreground))',
-			},
-			secondary: {
-				DEFAULT: 'hsl(var(--secondary))',
-				foreground: 'hsl(var(--secondary-foreground))',
-			},
-			destructive: {
-				DEFAULT: 'hsl(var(--destructive))',
-				foreground: 'hsl(var(--destructive-foreground))',
-			},
-			muted: {
-				DEFAULT: 'hsl(var(--muted))',
-				foreground: 'hsl(var(--muted-foreground))',
-			},
-			accent: {
-				DEFAULT: 'hsl(var(--accent))',
-				foreground: 'hsl(var(--accent-foreground))',
-			},
-			popover: {
-				DEFAULT: 'hsl(var(--popover))',
-				foreground: 'hsl(var(--popover-foreground))',
-			},
-			card: {
-				DEFAULT: 'hsl(var(--card))',
-				foreground: 'hsl(var(--card-foreground))',
-			},
-		},
-		borderRadius: {
-			lg: `var(--radius)`,
-			md: `calc(var(--radius) - 2px)`,
-			sm: 'calc(var(--radius) - 4px)',
-		},
-		keyframes: {
-			'accordion-down': {
-				from: { height: 0 },
-				to: { height: 'var(--radix-accordion-content-height)' },
-			},
-			'accordion-up': {
-				from: { height: 'var(--radix-accordion-content-height)' },
-				to: { height: 0 },
-			},
-		},
-		animation: {
-			'accordion-down': 'accordion-down 0.2s ease-out',
-			'accordion-up': 'accordion-up 0.2s ease-out',
-		},
-	},
-};
-export const plugins = [require('tailwindcss-animate')];
+	plugins: [require('tailwindcss-animate')]
+}
