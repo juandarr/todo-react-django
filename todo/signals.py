e91461ce2c890125b7fe85d429183c2cef0ed6db
Juan Rios <juandarr@proton.me>
2025-02-23 23:47:21 -0500

    add: signal to assign id of new todo to its index field; refactor: function to assign id of new list to its index field

--- todo/signals.py
+++ todo/signals.py
@@ -1,13 +1,20 @@
 from django.dispatch import Signal
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import List
+from .models import List, Todo
 
 user_created = Signal()
 
 @receiver(post_save, sender=List)
-def set_position_to_id(sender, instance, created, **kwargs):
+def set_list_index_to_id(sender, instance, created, **kwargs):
     if created:
-        # Set index to id after the item is created
+        # Set index to id after the new List instance is created
+        instance.index = instance.id
+        instance.save()
+
+@receiver(post_save, sender=Todo)
+def set_todo_index_to_id(sender, instance, created, **kwargs):
+    if created:
+        # Set index to id after the new Todo instance is created
         instance.index = instance.id
         instance.save()
