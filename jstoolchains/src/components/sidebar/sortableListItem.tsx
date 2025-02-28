b94a785e33f3efef467d7207ca5312ce29358f1e
Juan Rios <juanrios@tuta.io>
2025-02-28 18:37:31 -0500

    add: new button element to drag and drop whole item as a drag handler

--- jstoolchains/src/components/sidebar/sortableListItem.tsx
+++ jstoolchains/src/components/sidebar/sortableListItem.tsx
@@ -1,59 +1,64 @@
 import React from 'react';
-import {useSortable} from '@dnd-kit/sortable';
-import {CSS} from '@dnd-kit/utilities';
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 import EditModalList from '../modals/editModalList';
 import { SortableListItemProps } from '../../lib/customTypes';
+import { Apple } from 'iconsax-react';
 
-export default function SortableListItem({list, currentView, changeCurrentView, deleteList, editList}:SortableListItemProps) {
-  if (list.index === 1 || list.archived === true) {
-    return null;
-  }
-  const {
-    attributes,
-    listeners,
-    setNodeRef,
-    transform,
-    transition,
-  } = useSortable({
-    id: list.id as number
-  });
-  
-  const style = {
-    transform: CSS.Transform.toString(transform),
-    transition,
-  };
-  
-  return (
-    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
-        <div key={list.id} className='parent flex items-center justify-between'>
-                        <button
-                            className={`flex flex-1 cursor-pointer justify-start ${
-                                currentView.id === list.id
-                                    ? 'list-active-selected rounded-md bg-cyan-200 font-semibold'
-                                    : ''
-                            } lists-active truncate rounded-xl p-1 pl-2 text-base`}
-                            onClick={() => {
-                                changeCurrentView(list.id as number);
-                            }}>
-                            {list.title}
-                        </button>
-                        <div
-                            id={`list-${list.id}`}
-                            className={`hidden-child flex items-center justify-end 
+export default function SortableListItem({
+	list,
+	currentView,
+	changeCurrentView,
+	deleteList,
+	editList,
+}: SortableListItemProps) {
+	if (list.index === 1 || list.archived === true) {
+		return null;
+	}
+	const { attributes, listeners, setNodeRef, transform, transition } =
+		useSortable({
+			id: list.id as number,
+		});
+
+	const style = {
+		transform: CSS.Transform.toString(transform),
+		transition,
+	};
+
+	return (
+		<div ref={setNodeRef} style={style} >
+			<div key={list.id} className='parent flex items-center justify-between'>
+				<button {...attributes} {...listeners}>
+					<Apple size='28' color='#FF8A65' />
+				</button>
+				<button
+					className={`flex flex-1 cursor-pointer justify-start ${
+						currentView.id === list.id
+							? 'list-active-selected rounded-md bg-cyan-200 font-semibold'
+							: ''
+					} lists-active truncate rounded-xl p-1 pl-2 text-base`}
+					onClick={() => {
+						changeCurrentView(list.id as number);
+					}}>
+					{list.title}
+				</button>
+				<div
+					id={`list-${list.id}`}
+					className={`hidden-child flex items-center justify-end 
                             ${currentView.id === list.id ? 'list-actions-selected' : ''}`}>
-                            <span className='ml-2'></span>
-                            <EditModalList
-                                editList={editList}
-                                listData={{
-                                    id: list.id as number,
-                                    title: list.title,
-                                    archived: list.archived as boolean,
-                                }}
-                                parentId={`list-${list.id}`}
-                                deleteFunction={deleteList}
-                            />
-                        </div>
-                    </div>
-    </div>
-  );
+					<span className='ml-2'></span>
+					<EditModalList
+						editList={editList}
+						listData={{
+							id: list.id as number,
+							title: list.title,
+							archived: list.archived as boolean,
+						}}
+						parentId={`list-${list.id}`}
+						deleteFunction={deleteList}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
