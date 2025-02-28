12df2b0fab4315fa59f72600022fcc3cc88a63b0
Juan Rios <juanrios@tuta.io>
2025-02-27 23:45:20 -0500

    add: import drag and drop libraries in tasklist; also draft of functions required for drag and drop functionality to work

--- jstoolchains/src/components/taskview/tasklist.tsx
+++ jstoolchains/src/components/taskview/tasklist.tsx
@@ -3,6 +3,25 @@ import React from 'react';
 import type { TaskListProps } from '../../lib/customTypes';
 import TaskItem from './taskitem';
 
+/*Drag and drop imports*/
+import {
+	DndContext, 
+	closestCenter,
+	KeyboardSensor,
+	PointerSensor,
+	MouseSensor,
+	useSensor,
+	useSensors,
+  } from '@dnd-kit/core';
+import {
+	arrayMove,
+	SortableContext,
+	sortableKeyboardCoordinates,
+	verticalListSortingStrategy,
+  } from '@dnd-kit/sortable';
+  
+import SortableTaskItem from './sortableTaskItem';
+
 export default function TaskList({
 	todos,
 	lists,
@@ -12,6 +31,69 @@ export default function TaskList({
 	editTodoFull,
 	isComplete,
 }: TaskListProps): React.JSX.Element {
+	/* TODO Part two of implementation, add drag and drop functionality, import libraries, add sensors etc. Follow instructions from Claude 3.5*/
+	/* Drag and drop definitions */
+		const sensors = useSensors(
+			useSensor(PointerSensor),
+			useSensor(KeyboardSensor, {
+			  coordinateGetter: sortableKeyboardCoordinates,
+			})
+			/*useSensor(MouseSensor, {
+				activationConstraint: {distance: 5}
+			}),*/
+		  );
+		
+		  async function handleDragEnd(event: any) {
+			const {active, over} = event;
+			
+			if (active.id !== over.id) {
+		
+				const oldIndex = lists.findIndex(i => i.id==active.id);
+				const newIndex = lists.findIndex(i => i.id==over.id);
+	
+				const newLists = arrayMove(lists, oldIndex, newIndex);
+				const tmpIndex = lists[newIndex].index;
+				
+				/* Update active and over lists */
+				let tmpLists: {id:number; index:number;}[] = [];
+				if (newIndex < oldIndex) {
+					for (let i = newIndex; i < oldIndex; i++) {
+						tmpLists.push({id:lists[i].id as number, index: (lists[i].index as number)+1 });
+						newLists[i+1].index = (newLists[i+1].index as number)+1;
+					}
+					tmpLists.push({id:lists[oldIndex].id as number, index: tmpIndex as number });
+					
+				}else if (newIndex > oldIndex) {
+					for (let i = newIndex; i > oldIndex; i--) {
+						tmpLists.push({id:lists[i].id as number, index: (lists[i].index as number)-1 });
+						newLists[i-1].index = (newLists[i-1].index as number)-1;
+					}
+					
+				}
+				tmpLists.push({id:lists[oldIndex].id as number, index: tmpIndex as number });
+				newLists[newIndex].index = tmpIndex;
+	
+				tmpLists.map(async list => {
+				try {
+							await clientList.listsPartialUpdate({
+								id: list.id as number,
+								patchedList:{ index: list.index }}
+							);
+	
+							console.log('List was patched!');
+							
+						} catch (error) {
+							console.log('There was an error updating the field in List');
+							throw error;
+						}
+					});
+			
+				dispatchLists({ type: 'changed', payload: newLists });
+			}
+		  }
+	
+
+
 	const taskList = todos.map((todo, idx: number) => {
 		return (
 			<li key={todo.id} id={`item-${todo.id}`}>
