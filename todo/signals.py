fa9c1b564020197670f76a5d49eed7cff5c82fe0
Juan Rios <juanrios@tuta.io>
2025-03-20 15:22:50 -0500

    remove: signal to initialize index field with id value

--- todo/signals.py
+++ todo/signals.py
@@ -11,10 +11,3 @@ def set_list_index_to_id(sender, instance, created, **kwargs):
         # Set index to id after the new List instance is created
         instance.index = instance.id
         instance.save()
-
-@receiver(post_save, sender=Todo)
-def set_todo_index_to_id(sender, instance, created, **kwargs):
-    if created:
-        # Set index to id after the new Todo instance is created
-        instance.index = instance.id
-        instance.save()
