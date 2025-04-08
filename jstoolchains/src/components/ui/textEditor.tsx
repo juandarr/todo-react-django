99efa941b43e8f5846b9763e82e49c156b7ede8a
Juan Rios <juanrios@tuta.io>
2025-04-08 03:15:43 -0500

    remove: console log line from textEditor ui component

--- jstoolchains/src/components/ui/textEditor.tsx
+++ jstoolchains/src/components/ui/textEditor.tsx
@@ -55,7 +55,6 @@ export default function TextEditor({
 			),
 		[]);
 
-	console.log('EDITOR: ', editor.storage.characterCount.characters(), characterCountExtension?.storage.characters());
 	return (
 		<div className='relative mb-2 ml-4 mr-4 mt-2 flex flex-1 flex-col focus-within:rounded-lg focus-within:outline'>
 			<div className='flex max-h-[20vh] items-center justify-start overflow-y-auto rounded-t-lg bg-sky-50 px-1 py-1 text-sm text-gray-900'>
