f8bf62d76df5459fac1d201acb7af89141ce734c
Juan Rios <juanrios@tuta.io>
2025-04-15 03:46:30 -0500

    add: css instructions to define outline with a style in taskForm component

--- jstoolchains/src/components/taskview/taskform.tsx
+++ jstoolchains/src/components/taskview/taskform.tsx
@@ -60,7 +60,7 @@ export default function TaskForm({
 				<input
 					type='text'
 					name='title'
-					className='mt-0 h-10 flex-1 rounded-xl bg-gray-300 px-4 py-3 text-gray-900 placeholder:text-gray-500'
+					className='mt-0 h-10 flex-1 rounded-xl bg-gray-300 px-4 py-3 text-gray-900 placeholder:text-gray-500 focus-within:outline focus-within:outline-2 focus-within:outline-violet-500'
 					id='todoText'
 					value={newTodo.title}
 					onChange={(e) => {
