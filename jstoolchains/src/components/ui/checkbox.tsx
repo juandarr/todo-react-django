2f56ec0469fef6fbc785ea520166d7c6018db507
Juan Rios <juanrios@tuta.io>
2025-04-19 22:33:08 -0500

    improve: style when showing tasks in tasklist, style of checkbox in tasks

--- jstoolchains/src/components/ui/checkbox.tsx
+++ jstoolchains/src/components/ui/checkbox.tsx
@@ -13,13 +13,13 @@ const Checkbox = React.forwardRef<
 	<CheckboxPrimitive.Root
 		ref={ref}
 		className={cn(
-			'peer h-6 w-6 shrink-0 rounded-lg border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
+			'peer h-7 w-7 shrink-0 rounded-lg border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
 			className,
 		)}
 		{...props}>
 		<CheckboxPrimitive.Indicator
 			className={cn('flex items-center justify-center text-current')}>
-			<Check className='h-6 w-6' />
+			<Check className='text-cyan-500' />
 		</CheckboxPrimitive.Indicator>
 	</CheckboxPrimitive.Root>
 ));
