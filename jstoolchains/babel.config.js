16c945587cd5126fbe65f6f4488ae5799226d58f
Juan Rios <juanrios@tuta.io>
2025-04-22 15:49:38 -0500

    update: target babel to React 19 and updated react compiler and eslint-plugin-react-compiler to 19.0-rc

--- jstoolchains/babel.config.js
+++ jstoolchains/babel.config.js
@@ -1,11 +1,13 @@
 // jstoolchains/babel.config.js
+const ReactCompilerConfig = {
+	target: '19',
+};
+
 module.exports = {
-    presets: [
-      '@babel/preset-env',
-      ['@babel/preset-react', { runtime: 'automatic' }],
-      '@babel/preset-typescript',
-    ],
-    plugins: [
-      'babel-plugin-react-compiler',
-    ],
-  };
+	presets: [
+		'@babel/preset-env',
+		['@babel/preset-react', { runtime: 'automatic' }],
+		'@babel/preset-typescript',
+	],
+	plugins: ['babel-plugin-react-compiler', ReactCompilerConfig],
+};
