b4a8b0c050b3f5de245c11ae3d2c0acf09f82245
Juan Rios <juanrios@tuta.io>
2025-04-26 22:54:24 -0500

    add: webpack bundle tracker dependency, optimizations such as vendor cache, single runtime and deterministic moduleIds

--- jstoolchains/webpack.config.js
+++ jstoolchains/webpack.config.js
@@ -1,6 +1,16 @@
 const path = require('path');
+const BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
+	entry: './src/index.tsx',
+	plugins: [
+		new BundleTracker({
+			// Path where webpack-stats.json will be generated
+			// Needs to be accessible by Django
+			path: path.resolve(__dirname, 'webpack-stats/'), // Example: Put it in a sibling dir
+			filename: 'webpack-stats.json', // Standard name
+		}),
+	],
 	module: {
 		rules: [
 			{
@@ -14,8 +24,23 @@ module.exports = {
 		extensions: ['.tsx', '.ts', '.js', '.jsx'],
 	},
 	output: {
-		filename: 'index-bundle.js', // output bundle file name
+		filename: '[name].[contenthash].js', // output bundle file name
 		path: path.resolve(__dirname, '../static/js'), // path to our Django static directory
 	},
 	devtool: 'eval-source-map',
+	optimization: {
+		moduleIds: 'deterministic',
+		runtimeChunk: 'single',
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+
+					name: 'vendors',
+
+					chunks: 'all',
+				},
+			},
+		},
+	},
 };
