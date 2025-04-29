e4182a9505947785e18eecf6e9428a9fc180e02d
Juan Rios <juanrios@tuta.io>
2025-04-29 01:05:29 -0500

    format: new format, lists without trailing comma

--- jstoolchains/src/lib/api.ts
+++ jstoolchains/src/lib/api.ts
@@ -2,7 +2,7 @@ import {
 	TodosApi,
 	UsersApi,
 	ListsApi,
-	SettingsApi,
+	SettingsApi
 } from '../../../todo-api-client/apis/index';
 import { Configuration } from '../../../todo-api-client/runtime';
 import { getCookie } from './utils';
@@ -10,8 +10,8 @@ import { getCookie } from './utils';
 const apiConfig = new Configuration({
 	basePath: 'http://127.0.0.1:8000',
 	headers: {
-		'X-CSRFToken': getCookie('csrftoken'),
-	},
+		'X-CSRFToken': getCookie('csrftoken')
+	}
 });
 
 export const clientUser = new UsersApi(apiConfig);
