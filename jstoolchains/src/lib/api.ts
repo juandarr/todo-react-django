91131675075ad013369111ff54d1c358b073c141
Juan Rios <juanrios@tuta.io>
2025-06-01 18:46:32 -0500

    add: new api POST request sent to django backend with csrf token and password information

--- jstoolchains/src/lib/api.ts
+++ jstoolchains/src/lib/api.ts
@@ -18,3 +18,29 @@ export const clientUser = new UsersApi(apiConfig);
 export const clientTodo = new TodosApi(apiConfig);
 export const clientList = new ListsApi(apiConfig);
 export const clientSetting = new SettingsApi(apiConfig);
+
+export const changePasswordApi = async (
+	oldPassword: string,
+	newPassword1: string,
+	newPassword2: string
+) => {
+	const response = await fetch('http://127.0.0.1:8000/api/password_change/', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'X-CSRFToken': getCookie('csrftoken') || ''
+		},
+		body: JSON.stringify({
+			old_password: oldPassword,
+			new_password1: newPassword1,
+			new_password2: newPassword2
+		})
+	});
+
+	if (!response.ok) {
+		const errorData = await response.json();
+		throw new Error(errorData.detail || JSON.stringify(errorData));
+	}
+
+	return response.json();
+};
