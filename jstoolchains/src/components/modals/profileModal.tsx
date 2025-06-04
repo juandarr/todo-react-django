6008a6efb779522f2a9b37335c0dfe9c2ffa003c
Juan Rios <juanrios@tuta.io>
2025-06-03 23:03:06 -0500

    remove: comment not giving useful information

--- jstoolchains/src/components/modals/profileModal.tsx
+++ jstoolchains/src/components/modals/profileModal.tsx
@@ -64,7 +64,7 @@ export default function ProfileModal({
 						settings={settings}
 						editSetting={editSetting}
 					/>
-					<PasswordChangeModal /> {/* Use the new modal component */}
+					<PasswordChangeModal />
 					<a
 						href='/logout'
 						className='mb-2 flex items-center justify-start font-semibold text-violet-500 hover:text-violet-600'>
