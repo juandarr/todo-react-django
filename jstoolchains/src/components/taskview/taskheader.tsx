b7d1c0b31ee6f39821a046539bf500e4617107f9
Juan Rios <juanrios@tuta.io>
2025-06-10 03:13:25 -0500

    change: style of padding, width of taskheader component to achieve responsive UI

--- jstoolchains/src/components/taskview/taskheader.tsx
+++ jstoolchains/src/components/taskview/taskheader.tsx
@@ -8,11 +8,12 @@ export default function TaskListHeader({
 	fieldTask,
 	fieldActions,
 	isComplete,
-	items,
+	items
 }: TaskListHeaderProps): React.JSX.Element {
 	return (
-		<div className={`text-md flex rounded-xl bg-gray-100 py-3 font-bold`}>
-			<div className='w-2/12 px-6 text-center font-bold text-gray-700'>
+		<div
+			className={`text-md flex rounded-xl bg-gray-100 py-1.5 font-bold md:py-3`}>
+			<div className='w-2/12 pl-2 text-center font-bold text-gray-700'>
 				<span className='relative'>
 					{fieldDone}
 					<span className='absolute -bottom-1 left-[102%] text-xs font-semibold'>
@@ -20,11 +21,11 @@ export default function TaskListHeader({
 					</span>
 				</span>
 			</div>
-			<div className='flex-1 px-4 font-bold text-gray-700'>{fieldTask}</div>
-			<div className='w-[15%] pr-3 text-right text-gray-700 md:block'>
+			<div className='w-5/12 px-4 font-bold text-gray-700'>{fieldTask}</div>
+			<div className='flex-1 text-right text-gray-700 md:block'>
 				{fieldActions}
 			</div>
-			<div className='flex w-[5%] items-center justify-center text-rose-500 hover:text-rose-600'>
+			<div className='items-end justify-end text-rose-500 hover:text-rose-600'>
 				<ChevronDown
 					className={`collapsible ${isComplete ? 'active' : ''}`}
 					size='1.5rem'
